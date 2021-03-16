72c0059ecc3eca921e7768e598b9008cd8543cf8
RoB8080 <75566977@qq.com>
2021-03-16 23:52:39 +0800

    style: lint & fix code

--- src/config.ts
+++ src/config.ts
@@ -1,67 +1,67 @@
 export const base = {
-    "extends": [
-        "eslint:recommended"
+    'extends': [
+        'eslint:recommended',
     ],
-    "rules": {
-        "array-callback-return": "error",
-        "arrow-spacing": "error",
-        "block-spacing": "error",
-        "comma-dangle": ["error", "always-multiline"],
-        "curly": "error",
-        "eol-last": ["error", "always"],
-        "eqeqeq": "error",
-        "indent": ["error", 4, { "SwitchCase": 1 }],
-        "key-spacing": "error",
-        "max-lines": ["warn", { "max": 400 }],
-        "no-trailing-spaces": ["error", { "ignoreComments": true }],
-        "no-unneeded-ternary": "warn",
-        "no-useless-return": "error",
-        "object-curly-spacing": ["error", "always"],
-        "prefer-const": "error",
-        "prefer-destructuring": ["error", { "object": true, "array": false }],
-        "prefer-promise-reject-errors": "error",
-        "quotes": ["error", "single"],
-        "space-before-function-paren": ["error", {
-            "anonymous": "always",
-            "named": "never",
-            "asyncArrow": "always"
-        }]
-    }
+    'rules': {
+        'array-callback-return': 'error',
+        'arrow-spacing': 'error',
+        'block-spacing': 'error',
+        'comma-dangle': ['error', 'always-multiline'],
+        'curly': 'error',
+        'eol-last': ['error', 'always'],
+        'eqeqeq': 'error',
+        'indent': ['error', 4, { 'SwitchCase': 1 }],
+        'key-spacing': 'error',
+        'max-lines': ['warn', { 'max': 400 }],
+        'no-trailing-spaces': ['error', { 'ignoreComments': true }],
+        'no-unneeded-ternary': 'warn',
+        'no-useless-return': 'error',
+        'object-curly-spacing': ['error', 'always'],
+        'prefer-const': 'error',
+        'prefer-destructuring': ['error', { 'object': true, 'array': false }],
+        'prefer-promise-reject-errors': 'error',
+        'quotes': ['error', 'single'],
+        'space-before-function-paren': ['error', {
+            'anonymous': 'always',
+            'named': 'never',
+            'asyncArrow': 'always',
+        }],
+    },
 }
 export const ts = {
-    "parserOptions": {
-        "parser": "@typescript-eslint/parser"
+    'parserOptions': {
+        'parser': '@typescript-eslint/parser',
     },
-    "plugins": ["@typescript-eslint"],
-    "extends": [
-        "plugin:@typescript-eslint/recommended"
+    'plugins': ['@typescript-eslint'],
+    'extends': [
+        'plugin:@typescript-eslint/recommended',
     ],
-    "rules": {
-        "@typescript-eslint/explicit-module-boundary-types": "off",
-        "@typescript-eslint/no-namespace": ["error", { "allowDeclarations": true }]
-    }
+    'rules': {
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-namespace': ['error', { 'allowDeclarations': true }],
+    },
 }
 export const vue = {
-    "parser": "vue-eslint-parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        }
+    'parser': 'vue-eslint-parser',
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true,
+        },
     },
-    "extends": [
-        "plugin:vue/vue3-recommended"
+    'extends': [
+        'plugin:vue/vue3-recommended',
     ],
-    "rules": {
-        "vue/arrow-spacing": "error",
-        "vue/camelcase": "error",
-        "vue/block-spacing": "error",
-        "vue/brace-style": "error",
-        "vue/comma-dangle": ["error", "always-multiline"],
-        "vue/component-name-in-template-casing": ["error", "kebab-case"],
-        "vue/eqeqeq": "error",
-        "vue/html-indent": ["error", 4],
-        "vue/key-spacing": "error",
-        "vue/match-component-file-name": "error",
-        "vue/object-curly-spacing": ["warn", "always"]
-    }
+    'rules': {
+        'vue/arrow-spacing': 'error',
+        'vue/camelcase': 'error',
+        'vue/block-spacing': 'error',
+        'vue/brace-style': 'error',
+        'vue/comma-dangle': ['error', 'always-multiline'],
+        'vue/component-name-in-template-casing': ['error', 'kebab-case'],
+        'vue/eqeqeq': 'error',
+        'vue/html-indent': ['error', 4],
+        'vue/key-spacing': 'error',
+        'vue/match-component-file-name': 'error',
+        'vue/object-curly-spacing': ['warn', 'always'],
+    },
 }
--- src/peer-deps.ts
+++ src/peer-deps.ts
@@ -1,13 +1,13 @@
 export default {
     base: {
-        "eslint": ">=7"
+        'eslint': '>=7',
     },
     ts: {
-        "@typescript-eslint/eslint-plugin": ">=4.14.0",
-        "@typescript-eslint/parser": ">=4.14.0"
+        '@typescript-eslint/eslint-plugin': '>=4.14.0',
+        '@typescript-eslint/parser': '>=4.14.0',
     },
     vue: {
-        "eslint-plugin-vue": ">=7.4.0",
-        "vue-eslint-parser": ">=7.3.0"
+        'eslint-plugin-vue': '>=7.4.0',
+        'vue-eslint-parser': '>=7.3.0',
     },
 }
